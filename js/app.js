778616444d1ac2928457f64be27a61ffd35a0121
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-09 21:58:02 +0600

    local storage function added

--- js/app.js
+++ js/app.js
@@ -5,14 +5,65 @@ const getElement = (id) => {
 
 const handleSubmit = () => {
     // const input = document.getElementById('todo-text');
+
+    const todos = JSON.parse(localStorage.getItem("TODOS"));
     const inputText = getElement('todo-text').value;
 
+
+    // console.log(todos);
+
+    if (!todos) {
+        const todoList = [
+            {
+                title: inputText,
+                completed: false,
+            },
+        ];
+
+        localStorage.setItem("TODOS", JSON.stringify(todoList));
+    }
+    else {
+        const todoList = [
+            ...todos,
+            {
+                title: inputText,
+                completed: false,
+            },
+        ];
+        localStorage.setItem("TODOS", JSON.stringify(todoList));
+    };
+
+    render();
+};
+
+const render = () => {
+    const todos = JSON.parse(localStorage.getItem("TODOS"));
+    console.log(todos);
     const ul = getElement('todo-list');
 
-    const li = document.createElement('li');
-    // li.innerText = '';
-    li.innerText = inputText;
-    ul.appendChild(li);
+    // const li = document.createElement('li');
+    // // li.innerText = '';
+    // li.innerText = inputText;
+    // ul.appendChild(li);
+};
+
+render();
+
+
+    // const todoList = [
+    //     {
+    //         title: inputText,
+    //         completed: false,
+    //     }
+    // ]
+
+    // localStorage.setItem("TODOS", JSON.stringify(todoList));
+
+    // const ul = getElement('todo-list');
+
+    // const li = document.createElement('li');
+    // // li.innerText = '';
+    // li.innerText = inputText;
+    // ul.appendChild(li);
 
     // console.log(inputText);
-}
