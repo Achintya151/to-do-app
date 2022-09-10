67b24ce2a36e368e7a68739f651f13dfbffcccff
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-10 11:25:07 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const handleSubmit = () => {
     // const input = document.getElementById('todo-text');
 
     const todos = JSON.parse(localStorage.getItem("TODOS"));
+
     const inputText = getElement('todo-text').value;
 
 
@@ -52,11 +53,11 @@ const render = () => {
         li.innerText = noData;
         ul.appendChild(li);
     } else {
-        todos.forEach((item) => {
+        todos.forEach((item, index) => {
             const li = document.createElement('li');
             // li.innerText = item.title;
             li.innerHTML = `
-            <li class="flex justify-between py-2"><span>${item.title}</span><button for="my-modal-3" onClick="deleteItem()" class="btn btn-sm btn-circle text-right">✕</button></li>
+            <li class="flex justify-between py-2"><span>${item.title}</span><button for="my-modal-3" class="btn btn-sm btn-circle text-right">✕</button></li>
             `
             ul.appendChild(li);
         });
@@ -73,88 +74,7 @@ const handleClearAll = () => {
     render();
 }
 
-function deleteItem() {
-    event.target.parentElement.remove();
-
-    // let someArray = JSON.parse(localStorage.getItem('TODOS'));
-    // someArray = someArray.slice(0, x).concat(someArray.slice(-x));
-
-
-    // localStorage.removeItem('TODOS');
-
-    return;
-};
 
 render();
 
-// trying to add delete button
 
-// let storage1 = (function () {
-//     const inputText = getElement('todo-text').value;
-//     const todoList = [
-//         {
-//             title: inputText,
-//             completed: false,
-//         },
-//     ];
-
-//     return class storage1 {
-//         constructor(title, completed) {
-//             this[title] = todoList.title;
-//             this[completed] = todoList.completed;
-//         }
-
-
-//     }
-// }());
-
-// const deleteItem = () => {
-//     event.target.parentElement.remove();
-//     let a = event.target.parentElement.innerText;
-//     let word = a.split(' ');
-//     let item = new Array();
-//     let i = 0;
-//     let todos = JSON.parse(localStorage.getItem("TODOS"));
-//     window.localStorage.removeItem('TODOS');
-
-//     todos.forEach(val => {
-//         if (val.title === word[0]) {
-//             // console.log(val.title);
-//             const noData = "No data found.";
-//             // console.log(noData);
-//             const li = document.createElement('li');
-//             li.innerText = noData;
-//             ul.appendChild(li);
-//         }
-//         else {
-//             item[i] = new storage1(val.title, val.completed);
-//             i++;
-//         }
-//     });
-//     window.localStorage.setItem('TODOS', JSON.stringify(item));
-//     todos = [];
-//     // return;
-// }
-
-
-
-
-
-// rough
-    // const todoList = [
-    //     {
-    //         title: inputText,
-    //         completed: false,
-    //     }
-    // ]
-
-    // localStorage.setItem("TODOS", JSON.stringify(todoList));
-
-    // const ul = getElement('todo-list');
-
-    // const li = document.createElement('li');
-    // // li.innerText = ''; * hobe ul
-    // li.innerText = inputText;
-    // ul.appendChild(li);
-
-    // console.log(inputText);
